6db1ef97b04f5229e741c1c937a292ce52d9175b
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-08 15:03:25 -0700

    added to handlebars

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
+//  app's template engine of choice
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3004;
@@ -9,7 +12,9 @@ const PORT = process.env.PORT || 3004;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+// 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // turn on routes
 app.use(routes);
 
