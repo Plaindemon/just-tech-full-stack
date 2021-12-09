9291d2f3d17c4fda9de82c691203e5719d60e0c1
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-08 22:06:03 -0700

    added upvote

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -90,15 +90,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+
 
 router.put('/:id', (req, res) => {
   Post.update(
@@ -124,6 +116,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+});
 router.delete('/:id', (req, res) => {
   Post.destroy({
     where: {
